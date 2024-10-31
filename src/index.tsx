41b4216f0f986b3657a6e97012d14a1c24d7e27d
Stameskaaa <chta02@mail.ru>
2024-11-01 00:51:51 +0500

    second commit

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="todo-list">
         <App />
       </BrowserRouter>
     </Provider>
